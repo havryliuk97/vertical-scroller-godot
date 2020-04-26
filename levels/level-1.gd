c6578c0350a6174991665addbe022305b371376a
CossackHavrylo <cossackhavrylo@gmail.com>
2020-04-26 20:06:40 +0300

    fixed overlay not showing in respawn screen

--- levels/level-1.gd
+++ levels/level-1.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
 onready var respawn_timer := $respawn_timer
-onready var respawn_hud := $hud/hud_container/respawn_hud
-onready var respawn_counter := $hud/hud_container/respawn_hud/countdown
+onready var respawn_screen := $hud/respanw_screen
+onready var respawn_counter := $hud/respanw_screen/respawn_hud/countdown
 onready var player_controller := $player_controller
 
 
 func _ready():
 	player_controller.connect("player_killed", self, "_on_player_killed")
-	respawn_hud.hide()
+	respawn_screen.hide()
 
 
 func _process(delta):
@@ -17,10 +17,10 @@ func _process(delta):
 
 
 func _on_player_killed():
-	respawn_hud.show()
+	respawn_screen.show()
 	respawn_timer.start()
 
 
 func _on_respawn_timer_timeout():
-	respawn_hud.hide()
+	respawn_screen.hide()
 	player_controller.spawn_player()
